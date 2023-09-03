d0caf19f634301a46c6732871d5c7426f4d63798
Shubham Gupta <gshubham533@gmail.com>
2023-09-03 16:47:22 +0530

    feat: Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# d-id
-Python Library to access D-ID API 
+# D_ID Python Package
+
+The `D_ID` Python package is designed to interact with the D-ID API, allowing you to access various functionalities such as retrieving actor lists, creating text-to-video clips, and more.
+
+## Installation
+
+You can install the package via pip:
+
+```bash
+pip install d-id
+```
+
+## Getting Started
+
+1. Import the `Clips` class from the `D_ID` package:
+
+    ```python
+    from D_ID.D_ID import Clips
+    ```
+
+2. Initialize the `Clips` class with your D-ID API key:
+
+    ```python
+    api_key = "<your_api_key>"
+    clip = Clips(api_key)
+    ```
+
+## Usage Examples
+
+### Get Actor List
+
+```python
+actor_list = clip.get_actor_list()
+print(actor_list)
+```
+
+This function retrieves a list of actors available in the D-ID API.
+
+### Create Text-to-Video Clip
+
+```python
+text = "This is the text you want to convert into a video clip."
+presenter_id = "amy-jcwCkr1grs"
+driver_id = "uM00QMwJ9x"
+microsoft_voice_id = "en-US-JennyNeural"
+
+result = clip.create_text_to_video_clip(text, presenter_id, driver_id, microsoft_voice_id)
+print(result)
+```
+
+This function creates a video clip from the provided text using the specified presenter, driver, and Microsoft voice.
+
+### Additional Functions
+
+You can also use other functions provided by the `Clips` class to interact with the D-ID API, such as retrieving driver lists, presenter details, clips, and more.
+
+[comment]: <> ## Documentation
+
+[comment]: <> For detailed documentation on each function and its parameters, please refer to the inline comments in the `Clips` class definition within the package code.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Contributing
+
+If you would like to contribute to this project, please open an issue or submit a pull request.
+
+## Support
+
+If you encounter any issues or have questions about using this package, please [open an issue](https://github.com/Circuit-Minds/d-id/issues) on the GitHub repository.
---
+++ src/D_ID/D_ID.py
@@ -0,0 +1,107 @@
+import requests
+
+
+class __D_ID:
+    def __init__(self,api_key:str) -> None:
+        try:
+            split_key = api_key.split(':')
+            username = split_key[0]
+            password = split_key[1]
+        except Exception as e:
+            raise 'API key should be contain a colon. Format: username:password'
+        
+        self.username = username
+        self.password = password
+        self.base_url = 'https://api.d-id.com/'
+        self.headers = {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json',
+            'Authorization' : 'Basic ' + username + ':' + password
+        }
+        
+class Clips(__D_ID):
+    def __init__(self, api_key: str) -> None:
+        super().__init__(api_key)
+        self.endpoint = 'clips/'
+        self.url = self.base_url + self.endpoint
+        
+    def get_actor_list(self) -> dict:
+        url = self.url + 'actors' # https://api.d-id.com/clips/actors
+        
+        response  = requests.get(url=url,headers=self.headers)
+        
+        return response.json()
+    
+    def get_driver_list_of_an_actor(self,id:str) -> dict:
+        url = self.url + 'actors/'+ id +'/drivers' # https://api.d-id.com/clips/actors/<id>/drivers
+        
+        response = requests.get(url, headers=self.headers)
+        
+        return response.json()
+    
+    def get_list_of_the_presenter(self,limit:int=100) -> dict:
+        url = self.url + 'presenters/' + '?limit=' + str(limit) # https://api.d-id.com/clips/presenters
+        
+        response = requests.get(url, headers=self.headers)
+        
+        return response.json()
+    
+    def get_presenter_by_id(self,id:str) -> dict:
+        url = self.url + 'presenters/' +  str(id) # https://api.d-id.com/clips/presenters/<id>
+        
+        response = requests.get(url, headers=self.headers)
+        
+        return response.json()
+    
+    def get_clips(self,limit:int=100) -> dict:
+        url = self.url + '?limit=' +  str(limit) # https://api.d-id.com/clips?limit=100
+        
+        response = requests.get(url, headers=self.headers)
+        
+        return response.json()
+    
+    def get_clip_by_id(self,id:str) -> dict:
+        url = self.url +  str(id) # https://api.d-id.com/clips/<id>
+        
+        response = requests.get(url, headers=self.headers)
+        
+        return response.json()
+    
+    def delete_a_clip(self,id:str) -> dict:
+        url = self.url +  str(id) # https://api.d-id.com/clips/<id>
+        
+        response = requests.delete(url, headers=self.headers)
+        
+        return response.json()
+    
+    def get_presenter_driver_by_id(self,id:str) -> dict:
+        url = self.url + 'presenters/' +  str(id) + '/drivers' # https://api.d-id.com/clips/presenters/id/drivers
+        
+        response = requests.get(url, headers=self.headers)
+        
+        return response.json()
+    
+    
+    def create_text_to_video_clip(self,text:str,presenter_id:str='amy-jcwCkr1grs',driver_id:str='uM00QMwJ9x',microsoft_voice_id:str='en-US-JennyNeural') -> dict:
+
+        url = self.url # https://api.d-id.com/clips/
+
+        payload = {
+            "presenter_id": presenter_id,
+            "driver_id": driver_id,
+            "script": {
+                "type": "text",
+                "input": text,
+                
+                "subtitles": False,
+                "provider": {
+                    "type": "microsoft",
+                    "voice_id": microsoft_voice_id
+                },
+                "ssml": False,
+            },
+            "config": { "result_format": "mp4" }
+        }
+        response = requests.post(url, json=payload, headers=self.headers)
+
+        return response.json()
